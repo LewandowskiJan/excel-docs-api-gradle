3cd35292bf7241ac8c17f6aa164c7814d8aca589
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-07-31 06:45:06 +0200

    Google docs api - refactoring

--- src/main/java/SheetsQuickstart.java
+++ src/main/java/SheetsQuickstart.java
@@ -14,6 +14,7 @@ import com.google.api.services.sheets.v4.model.ValueRange;
 import com.lewandowski.excel.api.demo.dto.ExcelDto;
 import com.lewandowski.excel.api.demo.service.ExcelServiceDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.io.InputStream;
--- src/main/java/com/lewandowski/excel/api/demo/DemoApplication.java
+++ src/main/java/com/lewandowski/excel/api/demo/DemoApplication.java
@@ -1,11 +1,91 @@
 package com.lewandowski.excel.api.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sun.net.www.protocol.http.HttpURLConnection;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 
-@Component
+@SpringBootApplication
 public class DemoApplication {
+
+    private final String USER_AGENT = "Mozilla/5.0";
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(DemoApplication.class, args);
+
+      //  DemoApplication http = new DemoApplication();
+      //  http.sendGet();
+
+    }
+
+    // HTTP GET request
+    private void sendGet() throws Exception {
+
+        String url = "http://localhost:8080/show";
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    }
+
+    static class ThreadDemo extends Thread {
+        private Thread t;
+        private String threadName;
+
+        ThreadDemo( String name) {
+            threadName = name;
+            System.out.println("Creating " +  threadName );
+        }
+
+        public void run() {
+            System.out.println("Running " +  threadName );
+            try {
+                for(int i = 4; i > 0; i--) {
+                    DemoApplication http = new DemoApplication();
+                    http.sendGet();
+                    Thread.sleep(50);
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Thread " +  threadName + " interrupted.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread " +  threadName + " exiting.");
+        }
+
+        public void start () {
+            System.out.println("Starting " +  threadName );
+            if (t == null) {
+                t = new Thread (this, threadName);
+                t.start ();
+            }
+        }
+    }
 
 }
