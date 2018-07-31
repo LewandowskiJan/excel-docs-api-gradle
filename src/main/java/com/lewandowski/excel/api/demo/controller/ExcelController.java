26c8cc6a6e3afa1d182db312ffee2899a5b146d6
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-07-31 06:46:40 +0200

    Google docs api - controller

--- src/main/java/com/lewandowski/excel/api/demo/controller/ExcelController.java
+++ src/main/java/com/lewandowski/excel/api/demo/controller/ExcelController.java
@@ -1,27 +1,69 @@
 package com.lewandowski.excel.api.demo.controller;
 
 import com.lewandowski.excel.api.demo.dto.ExcelDto;
+import com.lewandowski.excel.api.demo.repository.ExcelRepository;
 import com.lewandowski.excel.api.demo.service.ExcelServiceDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class ExcelController {
 
+    private final Logger logger = LoggerFactory.getLogger(ExcelController.class);
+
     @Autowired
     ExcelServiceDto excelServiceDto;
 
-    @GetMapping("/show")
-    public void showPresentList() throws IOException, GeneralSecurityException {
+    @Autowired
+    ExcelRepository excelRepository;
 
-        List<List<ExcelDto>> result = excelServiceDto.getExcelDtoList();
+    @GetMapping("/show")
+    public String showPresentList(Model model) throws IOException, GeneralSecurityException {
+
+        logger.debug("index() is executed");
+
+        List<ExcelDto> result = excelServiceDto.getExcelDtoList();
 
         System.out.println(result);
+
+        List<ExcelDto> remoteList = new ArrayList<>();
+        List<ExcelDto> holidayList = new ArrayList<>();
+
+        int holidayCount = 0;
+        int remoteCount = 0;
+
+        for (ExcelDto excelDto : result) {
+            if (excelDto.isHoliday()) {
+                if (excelDto.getName().equals("")) {
+                } else {
+                    holidayList.add(excelDto);
+                    holidayCount++;
+                }
+            } else {
+                if (excelDto.getName().equals("")) {
+                } else {
+                    remoteList.add(excelDto);
+                    remoteCount++;
+                }
+            }
+        }
+
+        model.addAttribute("holidayList", holidayList);
+        model.addAttribute("remoteList", remoteList);
+        model.addAttribute("holidayCount", holidayCount);
+        model.addAttribute("remoteCount", remoteCount);
+
+
+        return "index";
 
     }
 
