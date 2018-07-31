0041525972264abcb888e77eb6f209de5c90b1e8
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-07-31 06:46:57 +0200

    Google docs api - excel dto

--- src/main/java/com/lewandowski/excel/api/demo/dto/ExcelDto.java
+++ src/main/java/com/lewandowski/excel/api/demo/dto/ExcelDto.java
@@ -1,13 +1,48 @@
 package com.lewandowski.excel.api.demo.dto;
 
-import org.springframework.stereotype.Component;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-@Component
+@Entity
 public class ExcelDto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String name;
 
+    private boolean remote;
+
+    private boolean holiday;
+
     public ExcelDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isRemote() {
+        return remote;
+    }
+
+    public void setRemote(boolean remote) {
+        this.remote = remote;
+    }
+
+    public boolean isHoliday() {
+        return holiday;
+    }
+
+    public void setHoliday(boolean holiday) {
+        this.holiday = holiday;
     }
 
     public String getName() {
@@ -22,6 +57,8 @@ public class ExcelDto {
     public String toString() {
         return "ExcelDto{" +
                 "name='" + name + '\'' +
+                ", remote='" + remote + '\'' +
+                ", holiday='" + holiday + '\'' +
                 '}';
     }
 }
