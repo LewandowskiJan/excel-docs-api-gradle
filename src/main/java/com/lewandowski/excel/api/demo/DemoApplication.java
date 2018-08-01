7907a8b0f56554aa77ba849ef0d165b0a2addc42
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-08-01 06:40:56 +0200

    Google docs api - main class refactor

--- src/main/java/com/lewandowski/excel/api/demo/DemoApplication.java
+++ src/main/java/com/lewandowski/excel/api/demo/DemoApplication.java
@@ -2,19 +2,12 @@ package com.lewandowski.excel.api.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import sun.net.www.protocol.http.HttpURLConnection;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
-    private final String USER_AGENT = "Mozilla/5.0";
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 }
