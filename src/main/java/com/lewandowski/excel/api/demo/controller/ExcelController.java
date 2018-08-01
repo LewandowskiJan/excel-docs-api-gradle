a5697600b9c4f20e1de92836258a65fbca6e9b8a
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-08-01 06:40:10 +0200

    Google docs api - controller refactor

--- src/main/java/com/lewandowski/excel/api/demo/controller/ExcelController.java
+++ src/main/java/com/lewandowski/excel/api/demo/controller/ExcelController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -16,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/")
 public class ExcelController {
 
     private final Logger logger = LoggerFactory.getLogger(ExcelController.class);
