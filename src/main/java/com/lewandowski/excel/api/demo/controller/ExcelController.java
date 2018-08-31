fbfc1344d89c447366817a6f16870b4a7dc1e611
JanLewandowsi <lewandowski.jan93@gmail.com>
2018-08-31 21:31:24 +0200

    Controller make the list split automatically

--- src/main/java/com/lewandowski/excel/api/demo/controller/ExcelController.java
+++ src/main/java/com/lewandowski/excel/api/demo/controller/ExcelController.java
@@ -1,5 +1,6 @@
 package com.lewandowski.excel.api.demo.controller;
 
+import com.google.api.client.util.DateTime;
 import com.lewandowski.excel.api.demo.dto.ExcelDto;
 import com.lewandowski.excel.api.demo.repository.ExcelRepository;
 import com.lewandowski.excel.api.demo.service.ExcelServiceDto;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,36 +36,139 @@ public class ExcelController {
 
         logger.debug("index() is executed");
 
+
         List<ExcelDto> result = excelServiceDto.getExcelDtoList();
+
+        ExcelDto dtoHolidayEmpty = new ExcelDto();
+        dtoHolidayEmpty.setName("");
+        dtoHolidayEmpty.setDate("");
+        dtoHolidayEmpty.setHoliday(true);
+
+        ExcelDto dtoRemoteEmpty = new ExcelDto();
+        dtoRemoteEmpty.setName("");
+        dtoRemoteEmpty.setDate("");
+        dtoRemoteEmpty.setRemote(true);
 
         //System.out.println(result);
 
         List<ExcelDto> remoteList = new ArrayList<>();
         List<ExcelDto> holidayList = new ArrayList<>();
 
-        int holidayCount = 0;
-        int remoteCount = 0;
+        int holidayCellCount = 0;
+        int remoteCellCount = 0;
 
         for (ExcelDto excelDto : result) {
             if (excelDto.isHoliday()) {
                 if (excelDto.getName().equals("")) {
                 } else {
                     holidayList.add(excelDto);
-                    holidayCount++;
+                    holidayCellCount++;
                 }
             } else {
                 if (excelDto.getName().equals("")) {
                 } else {
                     remoteList.add(excelDto);
-                    remoteCount++;
+                    remoteCellCount++;
                 }
             }
         }
+/*
+        if (holidayCellCount%6!=0){
+            while(holidayCellCount%6!=0){
+                holidayList.add(dtoHolidayEmpty);
+                holidayCellCount++;
+            }
+        }
 
-        model.addAttribute("holidayList", holidayList);
-        model.addAttribute("remoteList", remoteList);
-        model.addAttribute("holidayCount", holidayCount);
-        model.addAttribute("remoteCount", remoteCount);
+        if (remoteCellCount%6!=0){
+            while(remoteCellCount%6!=0){
+                remoteList.add(dtoRemoteEmpty);
+                remoteCellCount++;
+            }
+        }
+*/
+        List<ExcelDto> remoteList1 = new ArrayList<>();
+        List<ExcelDto> remoteList2 = new ArrayList<>();
+        List<ExcelDto> remoteList3 = new ArrayList<>();
+        List<ExcelDto> remoteList4 = new ArrayList<>();
+        List<ExcelDto> remoteList5 = new ArrayList<>();
+
+        int remoteCount = 0;
+        if (remoteList.size() > 0) {
+            for (ExcelDto excelDto : remoteList) {
+
+                if (remoteCount >= 0 && remoteList1.size() < 6) {
+                    remoteList1.add(excelDto);
+                }
+                if (remoteCount >= 6 && remoteList2.size() < 6) {
+                    remoteList2.add(excelDto);
+                }
+                if (remoteCount >= 12 && remoteList3.size() < 6) {
+                    remoteList3.add(excelDto);
+                }
+                if (remoteCount >= 18 && remoteList4.size() < 6) {
+                    remoteList4.add(excelDto);
+                }
+                if (remoteCount >= 24 && remoteList5.size() < 6) {
+                    remoteList5.add(excelDto);
+                }
+                remoteCount++;
+            }
+
+        }
+
+
+        List<ExcelDto> holidayList1 = new ArrayList<>();
+        List<ExcelDto> holidayList2 = new ArrayList<>();
+        List<ExcelDto> holidayList3 = new ArrayList<>();
+        List<ExcelDto> holidayList4 = new ArrayList<>();
+        List<ExcelDto> holidayList5 = new ArrayList<>();
+
+        int holidayCount = 0;
+        if (holidayList.size() > 0) {
+            for (ExcelDto excelDto : holidayList) {
+
+                if (holidayCount >= 0 && holidayList1.size() < 6) {
+                    holidayList1.add(excelDto);
+                }
+                if (holidayCount >= 6 && holidayList2.size() < 6) {
+                    holidayList2.add(excelDto);
+                }
+                if (holidayCount >= 12 && holidayList3.size() < 6) {
+                    holidayList3.add(excelDto);
+                }
+                if (holidayCount >= 18 && holidayList4.size() < 6) {
+                    holidayList4.add(excelDto);
+                }
+                if (holidayCount >= 24 && holidayList5.size() < 6) {
+                    holidayList5.add(excelDto);
+                }
+                holidayCount++;
+            }
+
+        }
+
+
+        Date dateTime = (Date.valueOf(LocalDate.now()));
+
+
+        model.addAttribute("now", dateTime);
+
+        model.addAttribute("holidayCount", holidayCellCount);
+        model.addAttribute("remoteCount", remoteCellCount);
+
+        model.addAttribute("remoteList1", remoteList1);
+        model.addAttribute("remoteList2", remoteList2);
+        model.addAttribute("remoteList3", remoteList3);
+        model.addAttribute("remoteList4", remoteList4);
+        model.addAttribute("remoteList5", remoteList5);
+
+
+        model.addAttribute("holidayList1", holidayList1);
+        model.addAttribute("holidayList2", holidayList2);
+        model.addAttribute("holidayList3", holidayList3);
+        model.addAttribute("holidayList4", holidayList4);
+        model.addAttribute("holidayList5", holidayList5);
 
 
         return "index";
